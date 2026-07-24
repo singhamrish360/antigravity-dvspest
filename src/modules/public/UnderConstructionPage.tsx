e7ec383019d1aa9755a179e1f8a1cecd43626522
Amrish Singh <singhamrish360@gmail.com>
2026-07-24 12:42:34 +0530

    Fix duplicate Layers identifier compilation error on UnderConstructionPage

--- src/modules/public/UnderConstructionPage.tsx
+++ src/modules/public/UnderConstructionPage.tsx
@@ -427,27 +427,3 @@ export const UnderConstructionPage: React.FC = () => {
     </div>
   );
 };
-
-interface LayersProps {
-  size?: number;
-  style?: React.CSSProperties;
-}
-
-const Layers: React.FC<LayersProps> = ({ size = 24, style }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width={size}
-    height={size}
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    style={style}
-  >
-    <polygon points="12 2 2 7 12 12 22 7 12 2" />
-    <polyline points="2 17 12 22 22 17" />
-    <polyline points="2 12 12 17 22 12" />
-  </svg>
-);
